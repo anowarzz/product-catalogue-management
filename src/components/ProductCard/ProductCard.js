2efffcbc387cc8e403317a77023cafbc315b24cd
anowarzz <anowarhosen444@gmail.com>
2023-01-19 06:33:28 +0600

    product description showed till 150 word and added read more button

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,28 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 
 const ProductCard = ({ singleProduct }) => {
   console.log(singleProduct);
 
-// Destructuring all the property of a single product
+  // Destructuring all the property of a single product
   const { category, description, id, image, price, rating, title } =
     singleProduct;
 
+  // State to toggle full product description
+
+  const [showFullDescription, setShowFullDescription] = useState(false);
+
   return (
-
-
-<Card className="p-2">
-      <Card.Img variant="top" src={image} style={ {height: '320px'}}/>
+    <Card className="p-2">
+      <Card.Img variant="top" src={image} style={{ height: "320px" }} />
       <Card.Body>
         <Card.Title className="fw-bold">{title}</Card.Title>
         <Card.Text>
-    {description}
+          {showFullDescription
+            ? description
+            : description?.slice(0, 150) + "..."}
         </Card.Text>
-        <Button variant="primary">Read More</Button>
+
+        {showFullDescription ? (
+          <Button
+            variant="primary"
+            onClick={() => setShowFullDescription(!showFullDescription)}
+          >
+            Hide Description
+          </Button>
+        ) : (
+          <Button
+            variant="primary"
+            onClick={() => setShowFullDescription(!showFullDescription)}
+          >
+            Read More
+          </Button>
+        )}
       </Card.Body>
     </Card>
-
-
   );
 };
 
