88bddb300c79537b58498bec7f00d2252bc04036
anowarzz <anowarhosen444@gmail.com>
2023-01-19 07:11:15 +0600

    all card size made same and postioned show more description button to the bottom of the card

--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -9,35 +9,49 @@ const ProductCard = ({ singleProduct }) => {
     singleProduct;
 
   // State to toggle full product description
-
   const [showFullDescription, setShowFullDescription] = useState(false);
 
   return (
-    <Card className="p-2">
-      <Card.Img variant="top" src={image} style={{ height: "320px" }} />
+    <Card className="p-2 h-100 position-relative shadow-sm">
+      <Card.Img
+        variant="top"
+        className="img-fluid"
+        src={image}
+        style={{ height: "350px" }}
+      />
       <Card.Body>
         <Card.Title className="fw-bold">{title}</Card.Title>
-        <Card.Text>
+
+        {/* If description is more than 150 words , than sliced it to 150 and if less than 150 full description shown  */}
+        <Card.Text className="py-2">
           {showFullDescription
             ? description
-            : description?.slice(0, 150) + "..."}
+            : description.length > 150
+            ? description.slice(0, 150) + "....."
+            : description}
         </Card.Text>
+        <p className="position-absolute bottom-0 start-10 fw-bold fs-6">
+          Price : ${price}
+        </p>
 
-        {showFullDescription ? (
-          <Button
-            variant="primary"
-            onClick={() => setShowFullDescription(!showFullDescription)}
-          >
-            Hide Description
-          </Button>
-        ) : (
-          <Button
-            variant="primary"
-            onClick={() => setShowFullDescription(!showFullDescription)}
-          >
-            Read More
-          </Button>
-        )}
+        {/* Toggling buttons to show full or less product description */}
+        <div className="position-absolute bottom-0 start-50 py-2">
+          {showFullDescription ? (
+            <Button
+              variant="danger"
+              onClick={() => setShowFullDescription(!showFullDescription)}
+            >
+              Show Less
+            </Button>
+          ) : (
+            <Button
+              variant="primary"
+              onClick={() => setShowFullDescription(!showFullDescription)}
+            >
+              Read More
+            </Button>
+          )}
+        </div>
       </Card.Body>
     </Card>
   );
