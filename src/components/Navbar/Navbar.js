03321f971aa8ff59cb71b44fb784aca51216475f
anowarzz <anowarhosen444@gmail.com>
2023-01-19 18:35:02 +0600

    hover effect added on navabar logo text

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -6,52 +6,64 @@ import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { ProductContext } from "../HomePage/HomePage";
+import './Navbar.css'
 
-function NavbarArea({loading, setLoading}) {
+
+
+function NavbarArea({ loading, setLoading }) {
   // all products from product context
   const [allProducts, setAllProducts, categories] = useContext(ProductContext);
 
-
-
-// state for search keywords
-// const [searchQuery, setSearchQuery] = useState("") 
+  // state for search keywords
+  // const [searchQuery, setSearchQuery] = useState("")
 
   // Filtering products according according category name selected
   const filteredProduct = (e) => {
     const categoryName = e.target.value;
 
-setLoading(true)
+    setLoading(true);
     fetch(`https://fakestoreapi.com/products/category/${categoryName}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
         setAllProducts(data);
-        setLoading(false)
+        setLoading(false);
       });
   };
 
+  // Displaying products according to search result
 
+  // const searchProduct = (e) => {
 
-// Displaying products according to search result
+  //   const searchQuery = e.target.value;
+  //   setSearchQuery(searchQuery)
 
-// const searchProduct = (e) => {
+  //   const searchProduct = allProducts.filter(product => product?.title?.includes(searchQuery))
+  //   setAllProducts(searchProduct)
 
-//   const searchQuery = e.target.value;
-//   setSearchQuery(searchQuery)
-
-
-
-//   const searchProduct = allProducts.filter(product => product?.title?.includes(searchQuery))
-//   setAllProducts(searchProduct)
-
-// }
-
+  // }
 
   return (
     <Navbar className="bg-light py-4 font-poppins" expand="md">
       <Container fluid>
         <Navbar.Brand className="text-dark fw-bold fs-4 ps-4 ">
-          <NavLink >Catalogue Management</NavLink>
+          <NavLink className="fs-3">
+            <span>C</span>
+            <span>a</span>
+            <span>t</span>
+            <span>a</span>
+            <span>l</span>
+            <span>o</span>
+            <span>g</span>
+            <span>u</span>
+            <span>e</span>  <span>M</span>
+            <span>a</span>
+            <span>n</span>
+            <span>a</span>
+            <span>g</span>
+            <span>e</span>
+            <span>r</span>
+          </NavLink>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
@@ -65,9 +77,7 @@ setLoading(true)
               id=""
               onChange={filteredProduct}
             >
-              <option >
-           All Category
-              </option>
+              <option>All Category</option>
 
               {categories?.map((category, i) => (
                 <option key={i} value={category}>
@@ -80,7 +90,6 @@ setLoading(true)
           {/* Searching product using product name keywords */}
           <Form className="d-flex flex-column flex-md-row mt-3 mt-md-0 gap-2 md-gap-0">
             <Form.Control
-           
               type="search"
               placeholder="Search Here"
               className="fs-7"
